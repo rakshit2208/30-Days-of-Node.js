510aeef2686437ea7d45111c889f6e1eca25f4c2
Rakshit Yadav <107808348+rakshit2208@users.noreply.github.com>
2024-02-14 15:38:50 +0530

    day14 done

---
+++ day14.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cachingMiddleware = require("./cachingMiddleware");
+
+const app = express();
+app.use(cachingMiddleware);
+
+app.get("/test", (req, res) => {
+  res.send("Response from server");
+});
+
+app.get("/test", (req, res) => {
+  res.send("Response from server");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+
+});
+
